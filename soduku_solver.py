4e47eb245c815184a7a9f0a60d7954b294892542
Hiren <Hiren@pikpok.com>
2021-03-25 10:54:00 +1300

    Fixed logic, script actually works now

--- soduku_solver.py
+++ soduku_solver.py
@@ -1,13 +1,14 @@
-
 board = [[5, 3, 0, 0, 7, 0, 0, 0, 0],
-        [6, 0, 0, 1, 9, 5, 0, 0, 0],
-        [0, 9, 8, 0, 0, 0, 0, 6, 0],
-        [8, 0, 0, 0, 6, 0, 0, 0, 3],
-        [4, 0, 0, 8, 0, 3, 0, 0, 1],
-        [7, 0, 0, 0, 2, 0, 0, 0, 6],
-        [0, 6, 0, 0, 0, 0, 2, 8, 0],
-        [0, 0, 0, 4, 1, 9, 0, 0, 5],
-        [0, 0, 0, 0, 8, 0, 0, 7, 9]]
+         [6, 0, 0, 1, 9, 5, 0, 0, 0],
+         [0, 9, 8, 0, 0, 0, 0, 6, 0],
+         [8, 0, 0, 0, 6, 0, 0, 0, 3],
+         [4, 0, 0, 8, 0, 3, 0, 0, 1],
+         [7, 0, 0, 0, 2, 0, 0, 0, 6],
+         [0, 6, 0, 0, 0, 0, 2, 8, 0],
+         [0, 0, 0, 4, 1, 9, 0, 0, 5],
+         [0, 0, 0, 0, 8, 0, 0, 7, 9]]
+
+solution = [[]]
 
 def print_board(board):
 
@@ -16,36 +17,40 @@ def print_board(board):
         for value in row:
             row_print += str(value) + " "
         print(row_print)
-
-def find_zero(board):
-    row_count = 0
-    col_count =  0  
+""" 
+def find_zero(board):  
     for row in board:
         for value in row:
             if value == 0:
-                return[row_count, col_count]
-            col_count += 1
-        row_count += 1
+                return[row, value]
+    return None """
+def find_zero(board):
+    for i in range(len(board)):
+        for j in range(len(board[0])):
+            if board[i][j] == 0:
+                return [i, j]
     return None
 
+
 #check's is the value in the grid, return false if it is, true if its not
-def grid_call(row, col, value, max_row, max_col):
+""" def grid_call(row, col, value, max_row, max_col):
        
     if row <= max_row:
         if col <=max_col:
-            row_counter = 0
-            col_counter = 0
+            
+            
             while True:
-                for row_counter in range(max_row):
-                    for col_counter in range(max_col):
-                        board_val = board[row_counter][col_counter]
+                for row in range(row, max_row):
+                    col = max_col - 3
+                    for col in range(col, max_col):
+                        board_val = board[row][col]
                         if value == board_val:
                             print("Returning false")
                             return False
-                        col_counter +=1
-                    row_counter +=1    
-                return True        
-
+                        col +=1
+                    row +=1 
+                row =  max_row -3   
+                return True       """  
 
 def is_valid(board, row, col, value):
     board_row =  board[row]
@@ -105,38 +110,56 @@ def is_valid(board, row, col, value):
         if col < 9 and col > 5:       
             if grid_call(row,col, value, 9,9) == True:
                 return True
+    return False 
+
+
+def is_valid(board, row, col, value):
+  # check if the input value already exited in row
+  if value in board[row]:
     return False
+  # if not, check if the input value already exited in column
+  for r in range(len(board)):
+    if value == board[r][col]:
+      return False
+  # if not, check if the input value already exites in 3x3 grid
+  for r in range(0,len(board),3):
+    for c in range(0, len(board),3):
+      grid = []
+      for r_ in range(0,3):
+        grid += board[r:r+3][r_][c:c+3]
+      if row in range(r,r+3) and col in range(c,c+3):
+        if value in grid:
+          return False
+ 
+  return True
 
 
-def solve(board):
+def solve(board, value_avoid=None):
     #base case 
     if find_zero(board) == None:
         print("Puzzle Solved!")
         print_board(board) #print solved board 
-        return board
+        return True
     else:
         #calls find_zero function which looks for a 0 on the board.
         empty_space = find_zero(board)
+        #x, y = empty_space
         x = empty_space[0]
         y = empty_space[1]
+        print("PRINTING BOARD BEFORE TRYING TO HIT VALUES")
         #recursive case Trys to fill in a value starting at 1, calls the is_valid 
-      
         for value in range(1,10):
-    
             if is_valid(board,x, y, value) == True:
-                x = empty_space[0]
-                y = empty_space[1]
                 board[x][y] = value #assign the new value to the board 
-                return solve(board) #recursurvly call again, with updated board.
-                
-                print(print_board(board))
-            if is_valid(board,x, y, value) == False:
-                print(print_board(board))
-                if value == 9: #have tried all values no valid solution backtrack 
-                    board[x][y-1] = 0 #set value back to 0 
-                    print_board(board) 
-                    return solve(board) #call solveboard again... problem is it's going to try the same value ... 
+                sol = solve(board)                                                                         
+                if sol:
+                    return True 
+                else:
+                    board[x][y] = 0
+            else:
+                continue
     return print("No Solution")
 
-
 solution = solve(board)
+print(solution)
+
